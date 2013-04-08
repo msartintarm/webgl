bf35ff1da517355c580f5c7e3d9ebc4f8e0696bc
Michael Sartin-Tarm <msartintarm@wisc.edu>
2013-04-08 13:47:40 -0500

    removed all files we no longer use in the project. (although it's possible i missed some. )

--- modelTest/Camera.js
+++
@@ -1,9 +0,0 @@
-function Light() {
-    this.object = new Sphere(0.5);
-}
-
-Floor.prototype.draw = function(gl_) {
-    GLmatrix.transform(rotatedLightPos);
-    this.object.draw(gl_);
-    GLmatrix.transform(-rotatedLightPos);
-}
