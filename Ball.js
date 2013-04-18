7abe45c800d52f1436b3546d1498e005defe9803
Christopher Richard Aberger <caberger@tux-40.cae.wisc.edu>
2013-04-17 21:36:31 -0500

    Fixed collision detection.  Made it simpler.

--- Ball.js
+++ Ball.js
@@ -27,61 +27,15 @@ Ball.prototype.detectCollision = function(oldPosition, newPosition){
     var x_dir = newPosition[0] - oldPosition[0];
     var z_dir = newPosition[2] - oldPosition[2];
 
-    //moving in the +x direction
-    //and in proper z plane for collision
-    //need to check two radius's out
-    //remember you are a ball as well.
-
-    var hit_x = false;
-    var hit_z = false;
-
-    //check to see if we cross sphere on x axis
-    if(x_dir > 0){
-	if(oldPosition[0]-(this.radius) <= this.position[0] &&
-	   this.position[0] <= newPosition[0]+this.radius){
-//	    alert("HIT X Pos");
-	    hit_x = true;;
-	}
-    }
-    else if(x_dir < 0){
-	if((newPosition[0]-this.radius) <= this.position[0] &&
-	   this.position[0] <= (oldPosition[0]+this.radius)){
-//	    alert("HIT X Neg");
-	    hit_x = true;
-	}
-    } 
-
     //check to see if we cross sphere on z axis
-    if(z_dir > 0){
-	if((oldPosition[2]-this.radius) <= this.position[1] &&
-	   this.position[1] <= (newPosition[2]+this.radius)){
-//	    alert("HIT Z Pos");
-	    hit_z = true;;
-	}
-    }
-    else if(z_dir < 0){
-	if((newPosition[2]-this.radius) <= this.position[1] &&
-	   this.position[1] <= (oldPosition[2]+this.radius)){
-//	    alert("HIT Z Neg");
-	    hit_z = true;
-	}
-    } 
+    var newPos = vec2.fromValues(newPosition[0],newPosition[2]);
+    var ballPos = vec2.fromValues(this.position[0], this.position[1]);
 
-    //if we hit in z and are not moving in x we still need to check x
-    if(oldPosition[0]-(this.radius) <= this.position[0] &&
-       this.position[0] <= newPosition[0]+this.radius){
-	//	    alert("HIT X Pos");
-	hit_x = true;;
-    }
-    if(oldPosition[0]-(this.radius) <= this.position[0] &&
-       this.position[0] <= newPosition[0]+this.radius){
-	//	    alert("HIT X Pos");
-	hit_x = true;;
-    }
-    
+    //calculate distance
+    var distance = vec2.distance(newPos, ballPos);
 
-    //if both are true we have a collision
-    if(hit_x && hit_z){
+    //if we are within two radius' of ball we have a collision
+    if(distance < 2*this.radius){
 	alert("HIT");
 	return false;
     }
